35b74ac421670df09578a966281a5b69fb26fa75
a10305-5 <123>
2019-03-05 11:37:52 +0700

    map location

---
+++ app/src/main/java/fahsai/ruts/ac/th/location/MyTracking.java
@@ -0,0 +1,112 @@
+package fahsai.ruts.ac.th.location;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+
+public class MyTracking extends Service implements LocationListener {
+
+    private final Context context;
+
+    boolean isGPSEnable = false;
+    boolean isNetworkEnable = false;
+    boolean canGetLocation = false;
+
+    Location location;
+    protected LocationManager locationManager;
+
+    public MyTracking(Context context) {
+        this.context = context;
+    }
+
+    //@androidx.annotation.Nullable
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String Provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String Provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String Provider) {
+
+    }
+
+    public Location getLocation() {
+
+        try {
+
+            locationManager = (LocationManager)
+                    context.getSystemService(LOCATION_SERVICE);
+            isGPSEnable =
+                    locationManager.isProviderEnabled(locationManager.GPS_PROVIDER);
+            isNetworkEnable =
+                    locationManager.isProviderEnabled(locationManager.NETWORK_PROVIDER);
+            //canGetLocation =
+                    //locationManager.isProviderEnabled(locationManager.canGetLocation);
+
+            if (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED
+                    || ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED) {
+
+                if (isGPSEnable) {
+
+                    if (location == null) {
+
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                        10000, 10,this);
+
+                        if (locationManager != null) {
+                            location =
+                                    locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        }
+                    }
+                }
+
+                if (location == null) {
+                    if (isNetworkEnable) {
+
+                        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                                10000, 10, this);
+                        if (locationManager != null) {
+                            location =
+                                    locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        }
+                    }
+                }
+            }
+
+
+        } catch (Exception ex) {
+
+        }
+        return location;
+
+    }
+}
